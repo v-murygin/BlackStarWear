8640aa8ce9cef2eeae23916c226e4b9b0eea9948
Vladislav <muriginvlad@gmail.com>
2020-08-13 17:30:51 +0300

    Initial commit

---
+++ BlackStarWear/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlackStarWear
+//
+//  Created by Владислав on 13.08.2020.
+//  Copyright © 2020 Murygin Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
